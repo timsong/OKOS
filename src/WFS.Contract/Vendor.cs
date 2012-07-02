3fd62932aad8ca1249c8c4772949b74fbb8099d1
timsong <tim.h.song@gmail.com>
2012-07-01 23:32:26 -0600

    restructuring projects

---
+++ src/WFS.Contract/Vendor.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections.Generic;
+
+namespace WFS.Contract
+{
+    public class Vendor
+    {
+        public Vendor()
+        {
+            Stores = new List<Store>();
+            Menus = new List<Menu>();
+        }
+
+        public int VendorId { get; set; }
+        public string Name { get; set; }
+        public bool IsActive { get; set; }
+
+        public List<Store> Stores { get; set; }
+        public List<Menu> Menus { get; set; }
+    }
+}
---
+++ src/WFS.WebSite/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="WFS_WebSite.MvcApplication" Language="C#" %>
