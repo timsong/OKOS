b5aef0f7952109aedeaf5726c9a304dd7bf87d21
mick1313 <mickey@keewise.com>
2012-07-10 18:42:11 -0600

    added in MVC4

--- src/WFS.DataContext/WFSDataContext.Context.cs
+++ src/WFS.DataContext/WFSDataContext.Context.cs
@@ -25,16 +25,7 @@ namespace WFS.DataContext
             throw new UnintentionalCodeFirstException();
         }
     
-        public DbSet<aspnet_Applications> aspnet_Applications { get; set; }
-        public DbSet<aspnet_Membership> aspnet_Membership { get; set; }
-        public DbSet<aspnet_Paths> aspnet_Paths { get; set; }
-        public DbSet<aspnet_PersonalizationAllUsers> aspnet_PersonalizationAllUsers { get; set; }
-        public DbSet<aspnet_PersonalizationPerUser> aspnet_PersonalizationPerUser { get; set; }
-        public DbSet<aspnet_Profile> aspnet_Profile { get; set; }
-        public DbSet<aspnet_Roles> aspnet_Roles { get; set; }
         public DbSet<aspnet_SchemaVersions> aspnet_SchemaVersions { get; set; }
-        public DbSet<aspnet_Users> aspnet_Users { get; set; }
-        public DbSet<aspnet_WebEvent_Events> aspnet_WebEvent_Events { get; set; }
         public DbSet<Customer> Customers { get; set; }
         public DbSet<CustomerUser> CustomerUsers { get; set; }
         public DbSet<FoodCategory> FoodCategories { get; set; }
@@ -49,5 +40,10 @@ namespace WFS.DataContext
         public DbSet<VendorUser> VendorUsers { get; set; }
         public DbSet<WFSUser> WFSUsers { get; set; }
         public DbSet<WFSUserAddress> WFSUserAddresses { get; set; }
+        public DbSet<Application> Applications { get; set; }
+        public DbSet<Membership> Memberships { get; set; }
+        public DbSet<Profile> Profiles { get; set; }
+        public DbSet<Role> Roles { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
---
+++ src/WFS.Domain/Managers/MembershipManager.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Web.Security;
+
+namespace WFS.Domain.Managers
+{
+    public class MembershipManager
+    {
+        public void test()
+        {
+            
+        }
+    }
+}
