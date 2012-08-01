0dd0d42c5514498cd72a1fb7fc3e9fef0ae521a6
mikedhutchins <mike@mhutchins.com>
2012-08-01 08:31:13 -0600

    new views mvoed from home

---
+++ src/WFS.WebSite4/Controllers/GeneralController.cs
@@ -0,0 +1,39 @@
+﻿using System.Web.Mvc;
+
+namespace WFS.WebSite4.Controllers
+{
+    public class GeneralController : BaseController
+    {
+
+        public ActionResult AboutUs()
+        {
+            return View();
+        }
+
+        public ActionResult ContactUs()
+        {
+
+            return View();
+        }
+
+        public ActionResult Why()
+        {
+            return View();
+        }
+
+        public ActionResult HealthAndNutrition()
+        {
+            return View();
+        }
+
+        public ActionResult Testimonials()
+        {
+            return View();
+        }
+
+        public ActionResult Support()
+        {
+            return View();
+        }
+    }
+}
