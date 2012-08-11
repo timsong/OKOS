1ddffbbb4a2e74f76ddad45ccdb8f8b784741c20
mikedhutchins <mike@mhutchins.com>
2012-08-11 09:17:30 -0600

    x

---
+++ src/WFS.Contract/ReqResp/DeleteVendor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WFS.Framework;
+
+namespace WFS.Contract.ReqResp
+{
+	public class DeleteVendorRequest
+	{
+        public int VendorId { get; set; }
+	}
+
+    public class DeleteVendorResponse : BaseResponse
+    {
+
+    }
+}
