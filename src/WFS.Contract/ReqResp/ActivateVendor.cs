a65ff57dd9362c58b5a8362e48dbc40432c3f42a
mikedhutchins <mike@mhutchins.com>
2012-08-15 22:47:44 -0600

    lots

---
+++ src/WFS.Contract/ReqResp/ActivateVendor.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using WFS.Repository;
+
+namespace WFS.Contract.ReqResp
+{
+    public class ActivateVendorRequest
+    {
+        public int VendorId { get; set; }
+
+        public bool IsActive { get; set; }
+    }
+
+    public class ActivateVendorResponse : Result<bool>
+    {
+
+    }
+}
